e52488fe4c80123118818ca9f52ed82d830a88a5
v1nk03 <vovakonoval823@gmail.com>
2025-05-18 22:46:05 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -17,36 +17,46 @@ void Train::addCar(bool light) {
         first->prev = newTrainCar;
     }
 }
+
 int Train::getLength() {
     if (!first) return 0;
 
     Car* currentTrainCar = first;
+    int length = 0;
+    bool foundUnlit = false;
+
     if (!currentTrainCar->light) {
         currentTrainCar->light = true;
         countOp++;
     }
-    int length = 0;
-    while (true) {
+
+    while (!foundUnlit) {
         int traversalSteps = 0;
         do {
             currentTrainCar = currentTrainCar->next;
             traversalSteps++;
             countOp++;
         } while (!currentTrainCar->light && currentTrainCar != first);
-        if (currentTrainCar == first && !currentTrainCar->light) break;
-        currentTrainCar->light = false;
-        countOp++;
-        for (int i = 0; i < traversalSteps; i++) {
-            currentTrainCar = currentTrainCar->prev;
-            countOp++;
-        }
-        if (!currentTrainCar->light) {
-            length = traversalSteps;
+
+        if (currentTrainCar == first && !currentTrainCar->light) {
             break;
         }
+
+        if (!currentTrainCar->light) {
+            currentTrainCar->light = false;
+            countOp++;
+            for (int i = 0; i < traversalSteps; i++) {
+                currentTrainCar = currentTrainCar->prev;
+                countOp++;
+            }
+            length = traversalSteps;
+            foundUnlit = true;
+        }
     }
+
     return length;
 }
+
 int Train::getOpCount() {
     return countOp;
 }
